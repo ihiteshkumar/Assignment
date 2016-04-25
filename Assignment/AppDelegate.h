a2405a66b9bcb404402c818c343cfed0090be76a
Hitesh Kumar <hitesh.kumar@snapdeal.com>
2016-04-25 17:54:08 +0530

    Initial Commit

---
+++ Assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment
+//
+//  Created by Hitesh Kumar on 25/04/16.
+//  Copyright © 2016 Hitesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
